cfb888d6dda03369921f14f28db286c2ab27d9a6
Franz Rainier Adriano - BSIT <141382032+EasyRockr@users.noreply.github.com>
2025-10-07 23:17:45 +0800

    a

--- controller/convert_controller.py
+++ controller/convert_controller.py
@@ -4,5 +4,5 @@ class ConvertMoney:
     def __init__(self, source: str):
         self.rates_bll = RatesBll(source)
 
-    def convert(self, source: str, target: str, amount: float) -> float:
+    def convert(self, source: str, target: str, amount: float):
         return self.rates_bll.convert_amount(source, target, amount)
